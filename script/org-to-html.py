dd8c4ad8f28f6a0a2633bdbb63d5de6834223941
Chandler DeLoach <cdeloach@Chandlers-MBP.hsd1.co.comcast.net>
2023-05-29 10:44:43 -0600

    generate the intermediary JSON object

--- script/org-to-html.py
+++ script/org-to-html.py
@@ -1,41 +1,57 @@
+from pathlib import Path
+import re
+
 import bs4
 import orgparse
-
-# DEPRECATED loop through relevant tags
-#for tag in filter(lambda x: type(x) is bs4.element.Tag, soup.children)
-# 1. expect image -> drill down into tags to get image
-# 2. expect h1 and then non-heading tags -> RecipeNameSection 
-# 3. expect sets of h2 and then non-heading tags -> RecipeDetailSection
-
-# NOTE implement org parser
-# root = orgparse.load("recipes/recipe.org")
-# img_path = re.search("\[\[.*\]\]", root.get_body(format="raw")).group()[2:-2]
-# level1 = root.children[0]
-# recipe_name = level1.get_heading()
-# level1.properties ->
-# {'PrepTime': '30 minutes', 'CookTime': '35 minutes', 'Tags': 'american,comfort_food'}
-# recipe_props = level1.properties
-# recipe_props['Tags'] = recipe_props['Tags'].replace(" ","").split(",") # NOTE modify tags str -> list
-# for level2 in level1.children:
-#   section_header = level2.get_heading()
-#   NOTE process as unordered list
-#   if re.search("^\-\s", level2.get_body()):
-#     steps_raw = re.split("\-\s", level2.get_body())[1:]
-#     steps = normalize_orgl(steps_raw)
-#   NOTE process as ordered list
-#   elif re.search("^\d*\.\s", level2.get_body()):
-#     steps_raw = re.split("\d*\.\s", level2.get_body())[1:]
-#     steps = normalize_orgl(steps_raw)
-#   NOTE process as paragraph
-#   else:
-#     steps = level2.get_body.replace("\n", "")
-#   TODO parse level3 children
-#   while level.children != []:
-#     recurse(level)
-# TODO turn this into a function which builds a JSON object as intermediary
-# TODO generate the webpage from the JSON object with BeautifulSoup
 
 def normalize_orgl(org_list):
     # TODO replace with a "parse list" function that takes item matching regex as an arg
     "Remove excessive white space from raw org lists"
     return [re.sub("\s+", " ", i) for i in org_list if i != ""]
+
+def parse_orgnode(node):
+    "Take an OrgNode object from orgparse and recursively parse it, returning a JSON object"
+    def parse_as_list(body, entry_regex):
+        list_raw = re.split(entry_regex, body)[1:]
+        return [re.sub("\s+", " ", i) for i in list_raw if i != ""]
+
+    n_header = node.get_heading()
+    n_body = node.get_body()
+    if re.search("^\-\s", n_body):
+        body_parsed = parse_as_list(n_body, "\-\s")
+        body_format = "UnorderedList"
+    elif re.search("^\d*\.\s", n_body):
+        body_parsed = parse_as_list(n_body, "\d*\.\s")
+        body_format = "OrderedList"
+    else:
+        body_parsed = n_body.replace("\n", "")
+        body_format = "Paragraph"
+
+    children = ([parse_orgnode(child) for child in node.children]
+                if node.children != []
+                else None)
+    r_dict = {
+        "Header": n_header,
+        "Body": body_parsed,
+        "Format": body_format
+    }
+    if children:
+        r_dict["Children"] = children
+
+    return r_dict
+
+for orgfile in Path("recipes/").glob("*.org"):
+    root = orgparse.load(orgfile)
+    img_path = re.search("\[\[.*\]\]", root.get_body(format="raw")).group()[2:-2]
+    level1 = root.children[0]
+    page = {
+        "ImagePath": img_path,
+        "RecipeName": level1.get_heading(),
+        "Properties": level1.properties,
+        "Content": []
+    }
+    for level2 in level1.children:
+        page["Content"].append(parse_orgnode(level2))
+    print(page)
+
+# TODO generate the webpage from the JSON object with BeautifulSoup
